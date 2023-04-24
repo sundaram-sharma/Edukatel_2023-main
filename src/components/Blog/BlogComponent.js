f5d11f8bc63f61d48799075f8a8fc2c5acdbb65e
Sundaram Sharma <sundaram-sharma@hotmail.com>
2023-04-23 20:15:48 -0400

    Changes made to the UI

---
+++ src/components/Blog/BlogComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BlogComponent() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default BlogComponent
---
+++ src/components/Careers/JobLists.js
@@ -0,0 +1,10 @@
+export const JobLists = [
+    {
+        id: '1',
+        title: 'Home tutor',
+        description:'',
+        location: 'Wieruch',
+        date: 1988,
+      },
+  ];
+  
